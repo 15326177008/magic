98c5d12c900ba13bbea07f9c4f33ba90893abf54
liuhy-hub <15326177008@163.com>
2019-08-26 16:59:32 +0800

    new

--- demo/src/main/java/com/example/demo/controller/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
     @RequestMapping("/login")
     public String login(){
-        //return login page
         return "login";
     }
 }
